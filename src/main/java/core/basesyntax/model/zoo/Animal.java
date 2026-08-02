6c38caf41c426a538ad32a2d1770c5a8e50a00d8
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-08-02 16:48:04 +0200

    started project

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,21 @@
 package core.basesyntax.model.zoo;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+@Table(name = "animals")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "animal", discriminatorType = DiscriminatorType.STRING)
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long animalId;
     private int age;
     private String name;
 
@@ -18,5 +33,13 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(Long id) {
+        this.animalId = animalId;
     }
 }
